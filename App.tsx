0c230ada2763e16cd24453d109d9aef0f8a19809
Anthony Dreessen <anthonydreessen@gmail.com>
2022-03-24 12:49:11 -0700

    shell working with tabs

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
+import { Provider } from "react-native-paper";
 import { ErrorBoundary } from "./src/components/misc/ErrorBoundary";
 import { ContextStack } from "./src/contexts/ContextStack";
 import RootNavigator from "./src/nav/RootNavigator";
@@ -8,13 +9,15 @@ import { LoadingScreen } from "./src/screens/LoadingScreen";
 export default function App() {
   return (
     <NavigationContainer>
-      <LoadingScreen loading={false}>
-        <ErrorBoundary>
-          <ContextStack>
-            <RootNavigator />
-          </ContextStack>
-        </ErrorBoundary>
-      </LoadingScreen>
+      <Provider>
+        <LoadingScreen loading={false}>
+          <ErrorBoundary>
+            <ContextStack>
+              <RootNavigator />
+            </ContextStack>
+          </ErrorBoundary>
+        </LoadingScreen>
+      </Provider>
     </NavigationContainer>
   );
 }
--- src/nav/HomeStack.tsx
+++ src/nav/HomeStack.tsx
@@ -2,7 +2,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import React, { useContext } from "react";
 import { PremiumContext } from "../contexts/PremiumContext";
 import Theme from "../lib/Theme";
-import { HomeScreen } from "../screens/HomeScreen";
+import { TabNav } from "./TabNav";
 
 const Stack = createStackNavigator();
 
@@ -19,44 +19,43 @@ const HomeStack = () => {
         cardStyle: { backgroundColor: Theme.colors.background },
       }}
     >
-      <Stack.Screen name="Home" component={HomeScreen} />
-      {/*    <Stack.Screen
-  //       name="Plenti"
-  //       component={TabNav}
-  //       options={{ headerTitleAlign: "left", headerTitle: headerTitle }}
-  //     />
-  //     {/* <Stack.Screen
-  //       name="Chat"
-  //       component={ChatScreen}
-  //       options={{ title: "Chat", headerRight: () => <ConnectionStatus /> }}
-  //     />
-  //     <Stack.Screen
-  //       name="AddInventoryItem"
-  //       component={AddInventoryItemScreen}
-  //       options={{ title: "Add to Inventory" }}
-  //     />
-  //     <Stack.Screen
-  //       name="UpdateProfile"
-  //       component={UpdateProfileScreen}
-  //       options={({ navigation }) => ({
-  //         title: "Update Profile",
-  //       })}
-  //     /> */}
+      <Stack.Screen
+        name="Plenti"
+        component={TabNav}
+        options={{ headerTitleAlign: "left", headerTitle: headerTitle }}
+      />
       {/* <Stack.Screen
-  //       name="CatalogRequest"
-  //       component={CatalogRequestScreen}
-  //       options={{ title: "Catalog Request" }}
-  //     />
-  //     <Stack.Screen
-  //       name="ForgotPassword"
-  //       component={ForgotPasswordForm}
-  //       options={{ title: "Forgot Password" }}
-  //     />
-  //     <Stack.Screen
-  //       name="Store"
-  //       component={PremiumScreen}
-  //       options={{ title: "Plenti-Full" }}
-  //     /> */}
+        name="Chat"
+        component={ChatScreen}
+        options={{ title: "Chat", headerRight: () => <ConnectionStatus /> }}
+      />
+      <Stack.Screen
+        name="AddInventoryItem"
+        component={AddInventoryItemScreen}
+        options={{ title: "Add to Inventory" }}
+      />
+      <Stack.Screen
+        name="UpdateProfile"
+        component={UpdateProfileScreen}
+        options={({ navigation }) => ({
+          title: "Update Profile",
+        })}
+      />
+      <Stack.Screen
+        name="CatalogRequest"
+        component={CatalogRequestScreen}
+        options={{ title: "Catalog Request" }}
+      />
+      <Stack.Screen
+        name="ForgotPassword"
+        component={ForgotPasswordForm}
+        options={{ title: "Forgot Password" }}
+      />
+      <Stack.Screen
+        name="Store"
+        component={PremiumScreen}
+        options={{ title: "Plenti-Full" }}
+      />  */}
     </Stack.Navigator>
   );
 };
